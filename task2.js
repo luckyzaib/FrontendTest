ebd60f6e5e02c2fc141414b4c8f36741c017cc82
Muhammad Zohaib <luckyzaib13@gmail.com>
2024-02-11 12:16:31 +0500

    task 2 description

--- task2.js
+++ task2.js
@@ -1,3 +1,16 @@
+// Task 2
+
+// Users's data is stored in MongoDB. Your task is to compose a query returning a list of users,
+// satisfying the following conditions:
+// each user's email must have a domain, e.g. @example.org;
+// the last login attempt's latitude is in the (inclusive) range: [-90, 0];
+// the last login attempt's longitude is in the (inclusive) range: [-180, 0];
+// the number of unsuccessful login attempts is at least 1;
+// the role is neither admin nor client;
+// there are at least two active sessions and all of the active sessions last at least 8 hours.
+
+
+
 db.users.find({
     "mail": {  $text: {
         $search: '@example.org'} }, 
